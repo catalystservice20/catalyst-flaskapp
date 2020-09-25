9d7549bc09680255b2a95973c6deaf6e0246cf94
catalystservice20 <71809730+catalystservice20@users.noreply.github.com>
2020-09-25 16:28:59 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,14 +3,14 @@ from flask import Flask
 import os
 
 print("hello Catalyst")
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Flask inside Docker!!"
+# @app.route("/")
+# def hello():
+#     return "Flask inside Docker!!"
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True,host='0.0.0.0',port=port)
 
